7c98cf74d67d5709f042636d207e4c8f76f3d9f7
Max <max.arvid.anderson@gmail.com>
2018-10-15 12:33:37 -0400

    Update src/01-map.js

--- src/01-map.js
+++ src/01-map.js
@@ -13,4 +13,64 @@ let svg = d3
   .attr('height', height + margin.top + margin.bottom)
   .attr('width', width + margin.left + margin.right)
   .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+let projection = d3.geoMercator()
+let graticule = d3.geoGraticule()
+
+let path = d3.geoPath().projection(projection)
+
+var colorScale = d3.scaleSequential(d3.interpolateViridis).clamp(true)
+
+Promise.all([
+  d3.json(require('./data/world.topojson')),
+  d3.csv(require('./data/world-cities.csv'))
+])
+  .then(ready)
+  .catch(err => console.log('Failed on', err))
+
+function ready([json, datapoints]) {
+  // console.log(json.objects)
+  let countries = topojson.feature(json, json.objects.countries)
+  // console.log(countries)
+  let populationList = datapoints.map(d => d.population)
+  colorScale.domain(d3.extent(populationList))
+
+  svg
+    .selectAll('.country')
+    .data(countries.features)
+    .enter()
+    .append('path')
+    .attr('class', 'country')
+    .attr('d', path)
+    .attr('fill', 'black')
+    .attr('stroke', 'none')
+
+  svg
+    .selectAll('.cities')
+    .data(datapoints)
+    .enter()
+    .append('circle')
+    .attr('class', 'cities')
+    .attr('r', 0.75)
+    .attr('transform', d => {
+      let coords = projection([d.lng, d.lat])
+      return `translate(${coords})`
+    })
+    .attr('fill', d => colorScale(+d.population))
+
+  svg
+    .append('path')
+    .datum(graticule())
+    .attr('d', path)
+    .attr('stroke', 'white')
+    .attr('stroke-width', 0.5)
+    .lower()
+
+  svg
+    .append('rect')
+    .style('background-color', 'black')
+    .attr('height', height)
+    .attr('width', width)
+    .lower()
+}
